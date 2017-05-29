58805c567187e55de1e32ce2e6ac20c011409426
yanamadala1 <yanamadalamohan10@gmail.com>
2017-05-28 21:18:33 -0400

    included a template functionality remains same version 0.2.1

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -9,10 +9,11 @@ end
 package 'git' do
  action :install
 end
-file '/etc/motd' do
- content 'This server is Mohans property'
+
+template '/etc/motd' do
+ source 'motd.erb'
  action :create
-end
+end 
 
 service 'ntpd' do
  action [ :enable, :start ]
